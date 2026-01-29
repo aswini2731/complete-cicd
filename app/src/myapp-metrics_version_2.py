25541b7f94c7ca138256f3d1bc8bafeadcabf412
Sunita Sahu <aswinikumarpadhy@Aswinis-MacBook-Pro.local>
2026-01-29 23:56:37 +0530

    final

--- app/src/myapp-metrics_version_2.py
+++ app/src/myapp-metrics_version_2.py
@@ -1,13 +1,11 @@
 from flask import Flask
-import os
 
 application = Flask(__name__)
+app = application # This ensures 'app' is available for Gunicorn
 
 @app.route('/')
-def hello():
-    return "<h1>The Door is Open!</h1>"
+def home():
+    return "<h1>Calculator is Live!</h1>"
 
 if __name__ == "__main__":
-    # 0.0.0.0 tells Python to listen to Azure's requests
-    # port 8000 matches the 'target-port' we set in Azure
     app.run(host='0.0.0.0', port=8000)
