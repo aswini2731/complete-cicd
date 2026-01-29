9b8750f81de5a70a9e209ff82eac8bab18ab0b3c
Sunita Sahu <aswinikumarpadhy@Aswinis-MacBook-Pro.local>
2026-01-29 23:47:15 +0530

    loveubabe

--- app/src/myapp-metrics_version_2.py
+++ app/src/myapp-metrics_version_2.py
@@ -1,15 +1,13 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "<h1>Calculator is Running!</h1><p>To use: add /add/10/20 to the URL</p>"
-
-@app.route('/add/<int:a>/<int:b>')
-def add(a, b):
-    return f"<h1>Result: {a + b}</h1>"
+def hello():
+    return "<h1>The Door is Open!</h1>"
 
 if __name__ == "__main__":
-    # Port 8000 and 0.0.0.0 are MANDATORY for Azure
+    # 0.0.0.0 tells Python to listen to Azure's requests
+    # port 8000 matches the 'target-port' we set in Azure
     app.run(host='0.0.0.0', port=8000)
